a3f10350d38cdf7c72096442e46179629ee06ec8
Suprateek Yawagal <prateekyawagal@gmail.com>
2026-03-24 23:46:54 +0530

    added favicon with the help of realfavicongenerator

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,6 +27,12 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="GitVital" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={`${inter.variable} ${inter.className} antialiased`}>
         <ScrollPulse />
